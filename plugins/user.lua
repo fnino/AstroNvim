8d6c770f121d60729c3a2fdd722fdb2c1a596afb
Fernando NIÑO <fernando.nino@legos.obs-mip.fr>
2023-03-27 17:25:26 +0200

    Ajout de vim-fugitive et du code lua pour déclencher son chargement.

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,3 +1,20 @@
+-- InGitRepo source: https://github.com/wbthomason/packer.nvim/discussions/534
+
+local vim = vim
+
+local function check_git_repo()
+  local cmd = "git rev-parse --is-inside-work-tree"
+  if vim.fn.system(cmd) == "true\n" then
+    vim.api.nvim_exec_autocmds("User", { pattern = "InGitRepo" })
+    return true  -- removes autocmd after lazy loading git related plugins
+  end
+end
+
+vim.api.nvim_create_autocmd(
+  { "VimEnter", "DirChanged" },
+  { callback = function() vim.schedule(check_git_repo) end }
+)
+
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
@@ -9,5 +26,6 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
-  { "catppuccin/nvim", name = "catppuccin" }
+  {"tpope/vim-fugitive", event = 'User InGitRepo'},
+  {"catppuccin/nvim" },
 }
